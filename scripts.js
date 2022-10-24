7361341db0be10a689182e80ee60a2d77f1bbd1e
Midokhalil1 <midokhalil22@gmail.com>
2022-10-24 15:44:18 -0400

    css

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const family = [
+  {
+    name: "joe",
+    job: "mechanic",
+  },
+  {
+    dad: true,
+    kid1: "Mia",
+    pet: "🐠",
+  },
+];
+console.log("hey class here is my family", family);
+
+document.getElementById("contact").style.color = "purple";
+
+const handleColorChange = () => {
+  console.log("change color");
+};
+const handleAlert = () => {
+  alert("Alerting the class");
+};
